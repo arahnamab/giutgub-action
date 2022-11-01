9126ddac22073464b412668fa9f1b132fca552d6
arahnamab <113367668+arahnamab@users.noreply.github.com>
2022-11-01 14:48:11 -0400

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -3,8 +3,6 @@ import os
 def main():
   print("Hello from GitGub Action!")
   token = os.environ.get("AZURE_SECRET_TOKEN")
-  if not token:
-    raise RuntimeError("Not Azure SECRET provided")
   print("All good, we got an AZURE SECRET TOEKEN")
   
 if __name__ == "__main__":
