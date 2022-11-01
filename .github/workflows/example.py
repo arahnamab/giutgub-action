1758c48306fbd9606f62b8a337996a583355e46b
arahnamab <113367668+arahnamab@users.noreply.github.com>
2022-11-01 14:41:57 -0400

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -5,7 +5,7 @@ def main():
   token = os.environ.get("AZURE_SECRET_TOKEN")
   if not token:
     raise RuntimeError("Not Azure SECRET provided")
-  print("All good, we got aan AZURE SECRET TOEKEN" + str(token))
+  print("All good, we got an AZURE SECRET TOEKEN")
   
 if __name__ == "__main__":
   main()
