2e934d11bb0179e615cc54804881208ed4d55a2e
arahnamab <113367668+arahnamab@users.noreply.github.com>
2022-11-01 14:07:20 -0400

    Update example.py

--- .github/workflows/example.py
+++ .github/workflows/example.py
@@ -4,7 +4,7 @@ def main():
   print("Hello from GitGub Action!")
   token = os.environ.get("AZURE_SECRETE_TOKEN")
   if not token:
-    raise RuntimeError("AZURE_SECRETE_TOKEN" env var is not set!)
+    raise RuntimeError("AZURE_SECRETE_TOKEN env var is not set!)
   print("All good ! we found the sAzure secret token" + token)  
   
 if __name__ == "__main__":
